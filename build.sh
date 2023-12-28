de90a87698bd81326243a92fa94e4ef0ec160b4b
Mark Nash <abacefabmicroblast@hotmail.com>
2023-12-28 00:18:35 -0500

    Simplifying code

--- build.sh
+++ build.sh
@@ -1,19 +1,18 @@
-if command -v docker &> /dev/null; then
-    echo "✅ Docker is installed."
-else
-    echo "❌ Docker is not installied. Please install docker to continue. I am too lazy to write the commands to install docker here" >&2
+if ! command -v docker &> /dev/null; then
+    echo "❌ Docker is not installed. Please install Docker to continue."
     exit 1
 fi
 
 build_command="docker build . -t scn-librenms"
-permission_denied_string="permission denied"
+result=$($build_command 2>&1)
 
-result1=$($build_command 2>&1 >/dev/null)
-
-if [ $? -ne 0 ] && [[ "$result1" == *"$permission_denied_string"* ]]; then
-    eval "sudo $build_command"
+if [ $? -ne 0 ]; then
+	if [[ "$result" == *"permission denied"* ]]; then
+    		sudo $build_command || { echo "Unable to execute docker build command even after elevating permissions" >&2; exit 1; }
+	else
+		echo "unable to execute docker build command '$result'"
+	fi
 else
-    echo "Unable to execute docker build command: '$result1'"
+    echo "✅ Docker build successful."
 fi
-
 
